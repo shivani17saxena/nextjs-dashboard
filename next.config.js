c3ba1f47613997864db6c61bcd52e06e9ac7ae13
Milind Patel <milindpatel63@gmail.com>
2024-02-05 22:59:03 +0530

    probable fix for image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/nextjs-dashboard/public',
+    basePath: '/nextjs-dashboard',
 };
 
 module.exports = nextConfig;
